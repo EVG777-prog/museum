48c5570993147494ff42ca921d78d79e545b4ec6
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2021-07-31 19:49:45 +0300

    fix: add some features

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let [a, b] = [1, 2, 3];
-console.log(a, b);
-// { a, b } = { a: 1, b: 2, c: 3 };
-console.log(a, b);
+const progress1 = document.querySelector('#ppr');
+
+progress1.addEventListener('input', function() {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`;
+});
+
+const progress2 = document.querySelector('#vr');
+
+progress2.addEventListener('input', function() {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`
+})
