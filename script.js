0354ad07d59d35f51e5c155ab67956b0d9ac2146
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2021-08-02 19:25:06 +0300

    fix: make pixel perfect

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const progress1 = document.querySelector('#ppr');
 
 progress1.addEventListener('input', function() {
     const value = this.value;
-    this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`;
+    this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
 });
 
 const progress2 = document.querySelector('#vr');
 
 progress2.addEventListener('input', function() {
     const value = this.value;
-    this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`
+    this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
 })
